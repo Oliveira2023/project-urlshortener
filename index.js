950d2ecb3eb8a295f43d106beebb16845ec17a0f
Luciano <luciano.oliveira96@yahoo.com.br>
2024-07-25 23:42:29 -0300

    commit 2

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app = express();
+const dns = require('dns');
+const url = require('url');
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -24,24 +26,39 @@ app.get('/api/hello', function(req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-// app.post('/api/shorturl', function(req, res) {
-//   const url = req.body.url;
-//   res.json({ original_url: url, short_url: url.length });
-// }); 
-
-// app.get('/api/shorturl/:url', function(req, res) {
-//   const url = req.params.url;
-//   res.json({ original_url: url, short_url: url.length });
-// });
-
+var urlBody;
+var urlCurta;
 app.post('/api/shorturl', function(req, res) {
-  const url = req.body.url;
-  res.json({ original_url: url, short_url: url.length });
-
+  urlBody = req.body.url;
+  urlCurta = urlBody.length;
+  res.json({ original_url: urlBody, short_url: urlBody.length });
 });
+
 app.get('/api/shorturl/:url', function(req, res) {
-  const url = req.params.url;
-  res.json({ original_url: url, short_url: url.length });
+  const urlParam = req.params.url;
+  const urlE = urlBody + '/' + urlParam;
+  const submittedUrl = url.parse(urlE);
+  const hostname = submittedUrl.hostname;
+
+  dns.lookup(hostname, function(err, address) {
+    if (err) {
+      res.json({ error: 'invalid url' });
+    } else {
+      console.log('valid URL: ' + address);
+      if (urlParam == urlCurta) {
+      res.redirect(urlBody);
+      }
+    }
+  })
+
+  
+  // if (urlParam == urlCurta) {
+  //   res.redirect(url);
+  // } else {
+  //   res.json({ error: 'invalid URL' });
+  // }
+
+  
 });
 
 app.listen(port, function() {
